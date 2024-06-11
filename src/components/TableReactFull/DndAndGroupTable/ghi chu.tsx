747c7974a30f24087448a823f2471427aeed829a
Haha <haidanglink0@gmail.com>
2024-06-11 14:56:58 +0700

    up date mau tinh tong tung cot

--- src/components/TableReactFull/DndAndGroupTable/ghi chu.tsx
+++ src/components/TableReactFull/DndAndGroupTable/ghi chu.tsx
@@ -1,3 +1,15 @@
 header.column.columnDef.filterFn  ---> đường dẫn đến filterFn
 header.column.setFilterValue ---> đường dẫn đến setFilterValue
 column.columnDef.filterT --> lấy được giá trị của filterT: trên colums
+
+
+// tính tổng trên từng cột
+const calculateColumnSum = ( column, table) => {
+    return table.getFilteredRowModel().rows.reduce((sum, row) => {
+      const cellValue = row.getValue(column.id);
+      return typeof cellValue === 'number' ? sum + cellValue : sum;
+    }, 0);
+  };
+  
+  
+  footer: (info) => calculateColumnSum(info.column, info.table),
