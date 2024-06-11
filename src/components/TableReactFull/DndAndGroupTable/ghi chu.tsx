784a44fb39855e55ded5516e6f2425e36b483d76
haha <haidanglink0@gmail.com>
2024-06-12 00:03:57 +0700

    update 2

--- src/components/TableReactFull/DndAndGroupTable/ghi chu.tsx
+++ src/components/TableReactFull/DndAndGroupTable/ghi chu.tsx
@@ -1,36 +1,36 @@
-header.column.columnDef.filterFn  ---> đường dẫn đến filterFn
-header.column.setFilterValue ---> đường dẫn đến setFilterValue
-column.columnDef.filterT --> lấy được giá trị của filterT: trên colums
+header.column.columnDef.filterFn-- -> đường dẫn đến filterFn
+header.column.setFilterValue-- -> đường dẫn đến setFilterValue
+column.columnDef.filterT-- > lấy được giá trị của filterT: trên colums
 
 
 // tính tổng trên từng cột
-const calculateColumnSum = ( column, table) => {
-    return table.getFilteredRowModel().rows.reduce((sum, row) => {
-      const cellValue = row.getValue(column.id);
-      return typeof cellValue === 'number' ? sum + cellValue : sum;
-    }, 0);
-  };
-  
-  
-  footer: (info) => calculateColumnSum(info.column, info.table),
+const calculateColumnSum = (column, table) => {
+  return table.getFilteredRowModel().rows.reduce((sum, row) => {
+    const cellValue = row.getValue(column.id);
+    return typeof cellValue === 'number' ? sum + cellValue : sum;
+  }, 0);
+};
+
+
+footer: (info) => calculateColumnSum(info.column, info.table),
 
 
   // tìm leafHeaderGroupIndex
   const leafHeaderGroupIndex = table.getHeaderGroups().length - 1;
 // render tfoot ở mức leaf
-  <tfoot>
-          <tr>
-            {table.getHeaderGroups()[leafHeaderGroupIndex].headers.map(header => (
-              <td key={header.id} colSpan={header.colSpan}>
-                {header.isPlaceholder ? null : (
-                  <div>
-                    {header.column.columnDef.footer && flexRender(
-                      header.column.columnDef.footer,
-                      header.getContext()
-                    )}
-                  </div>
-                )}
-              </td>
-            ))}
-          </tr>
-        </tfoot>
+<tfoot>
+  <tr>
+    {table.getHeaderGroups()[leafHeaderGroupIndex].headers.map(header => (
+      <td key={header.id} colSpan={header.colSpan}>
+        {header.isPlaceholder ? null : (
+          <div>
+            {header.column.columnDef.footer && flexRender(
+              header.column.columnDef.footer,
+              header.getContext()
+            )}
+          </div>
+        )}
+      </td>
+    ))}
+  </tr>
+</tfoot>
