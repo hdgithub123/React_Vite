e2213ac32d6b76031017f691a2aab6f313ce2e5b
Haha <haidanglink0@gmail.com>
2024-08-07 17:39:59 +0700

    update

--- src/Nhap/NavbarSelect.tsx
+++ src/Nhap/NavbarSelect.tsx
@@ -3,20 +3,20 @@ import styles from './NavbarSelect.module.css';
 import { useState, useEffect, useRef } from 'react';
 
 
-export const DropableSelect = ({droptitle, children }) =>{
+export const DropableSelect = ({ droptitle, children }) => {
     const [open, setOpen] = useState(false);
-    const handelClick = ()=>{
+    const handelClick = () => {
         setOpen(!open)
     }
     return (
         <div className={styles.general}>
             <div className={styles.title} onClick={handelClick}> {droptitle} </div>
-            {open? <div className={styles.child}> {children} </div> : null}
+            {open ? <div className={styles.child}> {children} </div> : null}
         </div>
     );
 }
 
-export const DropableSelectHoverRight = ({droptitle, children }) =>{
+export const DropableSelectHoverRight = ({ droptitle, children }) => {
     return (
         <div className={styles.general_hover_Right}>
             <div className={styles.title_hover_Right}> {droptitle} </div>
@@ -25,7 +25,7 @@ export const DropableSelectHoverRight = ({droptitle, children }) =>{
     );
 }
 
-export const DropableSelectHoverbottom = ({droptitle, children }) =>{
+export const DropableSelectHoverbottom = ({ droptitle, children }) => {
     return (
         <div className={styles.general_hover_Bottom}>
             <div className={styles.title_hover_Bottom}> {droptitle} </div>
@@ -34,5 +34,30 @@ export const DropableSelectHoverbottom = ({droptitle, children }) =>{
     );
 }
 
+export const DropableSelectHover = ({ droptitle, children, position = 'top' }) => {
+    let childStyle
+    switch (position) {
+        case 'top':
+            childStyle = styles.child_hover_Top
+            break;
+        case 'botton':
+            childStyle = styles.child_hover_Bottom
+            break;
+        case 'left':
+            childStyle = styles.child_hover_Left
+            break;
+        case 'right':
+            childStyle = styles.child_hover_Right
+            break;
+        default:
+            childStyle = styles.child_hover_Bottom
+    }
 
+    return (
+        <div className={styles.general_hover}>
+            <div className={styles.title_hover}> {droptitle} </div>
+            <div className={childStyle}> {children} </div>
+        </div>
+    );
+}
 
