35fd7116fa885ed13992a578f92e37b33f0921bc
Haha <haidanglink0@gmail.com>
2024-08-09 09:19:25 +0700

    update

--- src/components/ReactTables/components/MainComponent/Others/ExplandingRow/ExplandingRow.tsx
+++ src/components/ReactTables/components/MainComponent/Others/ExplandingRow/ExplandingRow.tsx
@@ -1,22 +1,22 @@
 import { useState,useRef} from 'react';
 
 export const ExplandingRow = ({ table }) => {
-    const [showExpaned, setShowExpaned] = useState('Collapse All');
+    const [showExpaned, setShowExpaned] = useState('Expand All');
     const Explanded = useRef(false);
     const handleClick = ()=>{
         Explanded.current = !Explanded.current
-        table.setExpanded(Explanded.current);
         if (Explanded.current) {
             setShowExpaned('Collapse All')
+            table.setExpanded(true);
         }
         else{
             setShowExpaned('Expand All')
+            table.setExpanded(false);
         } 
     }
 
     return <>
         <div onClick={handleClick}>{showExpaned}</div>
-
     </>
 
 }
