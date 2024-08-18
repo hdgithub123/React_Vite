cfb8d92f176e1f3ee56bdaf446db09c7b0b3bee7
haha <haidanglink0@gmail.com>
2024-08-18 16:23:19 +0700

    dang thu

--- src/components/ReactTables/components/MainComponent/Others/getCommonPinningStyles.tsx
+++ src/components/ReactTables/components/MainComponent/Others/getCommonPinningStyles.tsx
@@ -1,23 +1,30 @@
 import { CSSProperties } from 'react'
 export const getCommonPinningStyles = (column): CSSProperties => {
     const isPinned = column.getIsPinned()
-    const isLastLeftPinnedColumn =
-      isPinned === 'left' && column.getIsLastColumn('left')
-    const isFirstRightPinnedColumn =
-      isPinned === 'right' && column.getIsFirstColumn('right')
-  
+  const isLastLeftPinnedColumn =
+    isPinned === 'left' && column.getIsLastColumn('left')
+  const isFirstLeftPinnedColumn =
+    isPinned === 'left' && column.getIsFirstColumn('left')
+
+  const isFirstRightPinnedColumn =
+    isPinned === 'right' && column.getIsFirstColumn('right')
+  // console.log("column.getStart('left')",column.getStart('left'))
+  // console.log("column",column)
     return {
       boxShadow: isLastLeftPinnedColumn
         ? '-4px 0 4px -4px gray inset'
         : isFirstRightPinnedColumn
           ? '4px 0 4px -4px gray inset'
           : undefined,
-      left: isPinned === 'left' ? `${column.getStart('left')}px` : undefined,
+      // left: isPinned === 'left' ? `${column.getStart('left')}px` : undefined,
+      left: isFirstLeftPinnedColumn ? `${column.getStart('left')+ 25}px` : isPinned === 'left' ? `${column.getStart('left')-9}px` : undefined,
+
       right: isPinned === 'right' ? `${column.getAfter('right')}px` : undefined,
       opacity: isPinned ? 0.95 : 1,
       position: isPinned ? 'sticky' : 'relative',
       width: column.getSize(),
       zIndex: isPinned ? 1 : 0,
+      backgroundColor: 'white',
     }
   }
 
