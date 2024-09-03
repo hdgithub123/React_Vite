3ab0c24b70c01ac629f07cd4d6cb0d62414f8025
haha <haidanglink0@gmail.com>
2024-09-03 21:09:52 +0700

    ud

--- src/commons/ReactRichTextEditor/ReactRichTextEditor_mau/ReactRichTextEditor_mau.tsx
+++ src/commons/ReactRichTextEditor/ReactRichTextEditor_mau/ReactRichTextEditor_mau.tsx
@@ -17,6 +17,14 @@ const ReactRichTextEditor_mau = () => {
         wrapperClassName="wrapper-class"
         editorClassName="editor-class"
         toolbarClassName="toolbar-class"
+        toolbar={{
+          inline: { inDropdown: true },
+          list: { inDropdown: true },
+          textAlign: { inDropdown: true },
+          link: { inDropdown: true },
+          history: { inDropdown: true },
+          image: { uploadCallback: uploadImageCallBack, alt: { present: true, mandatory: true } },
+        }}
       />
       <div className="code-view">
         <p>HTML View </p>
@@ -31,3 +39,25 @@ const ReactRichTextEditor_mau = () => {
 };
 
 export default ReactRichTextEditor_mau;
+
+
+function uploadImageCallBack(file) {
+  return new Promise(
+    (resolve, reject) => {
+      const xhr = new XMLHttpRequest();
+      xhr.open('POST', 'https://api.imgur.com/3/image');
+      xhr.setRequestHeader('Authorization', 'Client-ID 991a0e15ea25453');
+      const data = new FormData();
+      data.append('image', file);
+      xhr.send(data);
+      xhr.addEventListener('load', () => {
+        const response = JSON.parse(xhr.responseText);
+        resolve(response);
+      });
+      xhr.addEventListener('error', () => {
+        const error = JSON.parse(xhr.responseText);
+        reject(error);
+      });
+    }
+  );
+}
