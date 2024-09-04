fd4f30bfcfb99d52cd922f7ba368cc8761cac4b4
Haha <haidanglink0@gmail.com>
2024-09-04 09:33:49 +0700

    update tam

--- src/commons/ReactRichTextEditor/ReactRichTextEditor_mau/ReactRichTextEditor_mau.tsx
+++ src/commons/ReactRichTextEditor/ReactRichTextEditor_mau/ReactRichTextEditor_mau.tsx
@@ -1,63 +1,162 @@
-import React, { useState } from "react";
-import { convertToRaw, EditorState } from "draft-js";
-import { Editor } from "react-draft-wysiwyg";
-import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
-import "draft-js/dist/Draft.css";
-import draftToHtml from "draftjs-to-html";
+import React, { useState, useRef } from "react";
+import { Editor, EditorState, RichUtils, getDefaultKeyBinding } from "draft-js";
+// import "draft-js/dist/Draft.css";
 import './ReactRichTextEditor.css';
 
 const ReactRichTextEditor_mau = () => {
   const [editorState, setEditorState] = useState(EditorState.createEmpty());
+  const editorRef = useRef(null);
+
+  const focus = () => editorRef.current.focus();
+
+  const handleKeyCommand = (command, editorState) => {
+    const newState = RichUtils.handleKeyCommand(editorState, command);
+    if (newState) {
+      setEditorState(newState);
+      return true;
+    }
+    return false;
+  };
+
+  const mapKeyToEditorCommand = (e) => {
+    if (e.keyCode === 9 /* TAB */) {
+      const newEditorState = RichUtils.onTab(e, editorState, 4 /* maxDepth */);
+      if (newEditorState !== editorState) {
+        setEditorState(newEditorState);
+      }
+      return;
+    }
+    return getDefaultKeyBinding(e);
+  };
+
+  const toggleBlockType = (blockType) => {
+    setEditorState(RichUtils.toggleBlockType(editorState, blockType));
+  };
+
+  const toggleInlineStyle = (inlineStyle) => {
+    setEditorState(RichUtils.toggleInlineStyle(editorState, inlineStyle));
+  };
+
+  // Check if the editor has any content
+  let className = 'RichEditor-editor';
+  const contentState = editorState.getCurrentContent();
+  if (!contentState.hasText()) {
+    if (contentState.getBlockMap().first().getType() !== 'unstyled') {
+      className += ' RichEditor-hidePlaceholder';
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">Rich Text Editor Example</header>
-      <Editor
-        defaultEditorState={editorState}
-        onEditorStateChange={setEditorState}
-        wrapperClassName="wrapper-class"
-        editorClassName="editor-class"
-        toolbarClassName="toolbar-class"
-        toolbar={{
-          inline: { inDropdown: true },
-          list: { inDropdown: true },
-          textAlign: { inDropdown: true },
-          link: { inDropdown: true },
-          history: { inDropdown: true },
-          image: { uploadCallback: uploadImageCallBack, alt: { present: true, mandatory: true } },
-        }}
-      />
-      <div className="code-view">
-        <p>HTML View </p>
-        <textarea
-          className="text-area"
-          disabled
-          value={draftToHtml(convertToRaw(editorState.getCurrentContent()))}
+    <div className="RichEditor-root">
+      <BlockStyleControls editorState={editorState} onToggle={toggleBlockType} />
+      <InlineStyleControls editorState={editorState} onToggle={toggleInlineStyle} />
+      <div className={className} onClick={focus}>
+        <Editor
+          blockStyleFn={getBlockStyle}
+          customStyleMap={styleMap}
+          editorState={editorState}
+          handleKeyCommand={handleKeyCommand}
+          keyBindingFn={mapKeyToEditorCommand}
+          onChange={setEditorState}
+          placeholder="Tell a story..."
+          ref={editorRef}
+          spellCheck={true}
         />
       </div>
     </div>
   );
 };
 
-export default ReactRichTextEditor_mau;
+// Custom overrides for "code" style.
+const styleMap = {
+  CODE: {
+    backgroundColor: 'rgba(0, 0, 0, 0.05)',
+    fontFamily: '"Inconsolata", "Menlo", "Consolas", monospace',
+    fontSize: 16,
+    padding: 2,
+  },
+};
 
-
-function uploadImageCallBack(file) {
-  return new Promise(
-    (resolve, reject) => {
-      const xhr = new XMLHttpRequest();
-      xhr.open('POST', 'https://api.imgur.com/3/image');
-      xhr.setRequestHeader('Authorization', 'Client-ID 991a0e15ea25453');
-      const data = new FormData();
-      data.append('image', file);
-      xhr.send(data);
-      xhr.addEventListener('load', () => {
-        const response = JSON.parse(xhr.responseText);
-        resolve(response);
-      });
-      xhr.addEventListener('error', () => {
-        const error = JSON.parse(xhr.responseText);
-        reject(error);
-      });
-    }
-  );
+function getBlockStyle(block) {
+  switch (block.getType()) {
+    case 'blockquote': return 'RichEditor-blockquote';
+    default: return null;
+  }
 }
+
+const StyleButton = ({ onToggle, active, label, style }) => {
+  const onToggleHandler = (e) => {
+    e.preventDefault();
+    onToggle(style);
+  };
+
+  let className = 'RichEditor-styleButton';
+  if (active) {
+    className += ' RichEditor-activeButton';
+  }
+
+  return (
+    <span className={className} onMouseDown={onToggleHandler}>
+      {label}
+    </span>
+  );
+};
+
+const BLOCK_TYPES = [
+  { label: 'H1', style: 'header-one' },
+  { label: 'H2', style: 'header-two' },
+  { label: 'H3', style: 'header-three' },
+  { label: 'H4', style: 'header-four' },
+  { label: 'H5', style: 'header-five' },
+  { label: 'H6', style: 'header-six' },
+  { label: 'Blockquote', style: 'blockquote' },
+  { label: 'UL', style: 'unordered-list-item' },
+  { label: 'OL', style: 'ordered-list-item' },
+  { label: 'Code Block', style: 'code-block' },
+];
+
+const BlockStyleControls = ({ editorState, onToggle }) => {
+  const selection = editorState.getSelection();
+  const blockType = editorState.getCurrentContent().getBlockForKey(selection.getStartKey()).getType();
+
+  return (
+    <div className="RichEditor-controls">
+      {BLOCK_TYPES.map((type) => (
+        <StyleButton
+          key={type.label}
+          active={type.style === blockType}
+          label={type.label}
+          onToggle={onToggle}
+          style={type.style}
+        />
+      ))}
+    </div>
+  );
+};
+
+const INLINE_STYLES = [
+  { label: 'Bold', style: 'BOLD' },
+  { label: 'Italic', style: 'ITALIC' },
+  { label: 'Underline', style: 'UNDERLINE' },
+  { label: 'Monospace', style: 'CODE' },
+];
+
+const InlineStyleControls = ({ editorState, onToggle }) => {
+  const currentStyle = editorState.getCurrentInlineStyle();
+
+  return (
+    <div className="RichEditor-controls">
+      {INLINE_STYLES.map((type) => (
+        <StyleButton
+          key={type.label}
+          active={currentStyle.has(type.style)}
+          label={type.label}
+          onToggle={onToggle}
+          style={type.style}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default ReactRichTextEditor_mau;
